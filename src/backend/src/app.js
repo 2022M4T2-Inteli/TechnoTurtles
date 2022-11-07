b00855e964cf506daa694b489bfb647f848d8862
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-11-07 15:31:00 -0300

    api working

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+})
+
+app.listen(port, function(){
+    console.log('Api rodando na porta ' + port);
+});
