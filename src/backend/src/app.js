f8281f1879d53d3d4332f235e2d6d05d0d40851d
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-11-07 16:03:51 -0300

    post endpoint

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
 const port = 3000;
+
 
 app.get('/', function(req, res){
     res.send('Hello World');
 })
+
+app.post('/device', function(req, res){
+    console.log(req);
+    res.json({
+        "statusCode": 200
+    })
+});
 
 app.listen(port, function(){
     console.log('Api rodando na porta ' + port);
