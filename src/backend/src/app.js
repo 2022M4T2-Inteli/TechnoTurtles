248845f8241556c141eb38b4f92ea15368916602
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-11-08 14:58:59 -0300

    sqlite3

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res){
 })
 
 app.post('/device', function(req, res){
-    console.log(req);
+    console.log(req.body);
     res.json({
         "statusCode": 200
     })
