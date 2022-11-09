402f45abc26626564ead40e87253430df38807e7
manumorais <emanuele.martins@sou.inteli.edu.br>
2022-11-09 16:13:56 -0300

    main atualizado rfid, bt e wifi

--- src/hardware/rfid_detector/rfid_detector/rfid_detector.ino
+++ src/hardware/rfid_detector/rfid_detector/rfid_detector.ino
@@ -1,10 +1,35 @@
-//#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
+/*
+  Tecnoturtles - Bluetooth and Wi-Fi communication with ESP32
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+// Including all the libraries used for Wi-Fi and Bluetooth 
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <WiFi.h>
+#include "esp_wifi.h"
 #include <MFRC522.h>
 #include <SPI.h>
+#include <Wire.h>
 
-#define RFID_SS_SDA   47
-#define RFID_RST      14
+// Indicating the interval between scan payload broadcasting and create an object of the BLEscan class. 
+
+int scanTime = 5; // In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
+
+//rfid
+#define RFID_SS_SDA 47
+#define RFID_RST 14
 
 MFRC522 rfidBase = MFRC522(RFID_SS_SDA, RFID_RST);
 class LeitorRFID{
@@ -99,27 +124,95 @@ class LeitorRFID{
     };
 };
 LeitorRFID *leitor = NULL;
-//////////////////////////////
+
+void bluetoothSetup(){
+  // Routine to search BlueTooth devices 
+
+  Serial.println("Scanning...");
+
+  // Initiating the BLE module in ESP32 with no name
+
+  BLEDevice::init("");
+
+  //Setting parameters for the scanner
+
+  pBLEScan = BLEDevice::getScan(); // create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); // active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void bluetoothSearch(){
+  // Displays all devices found by bluetooth search
+  
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+}
+
+void wifiSearch(){
+  wifi_sta_list_t wifi_sta_list; // Creating a list of Wi-Fi stations
+  tcpip_adapter_sta_list_t adapter_sta_list; // Creating a list of TCP/IP stations
+
+  memset(&wifi_sta_list, 0, sizeof(wifi_sta_list)); // Clearing the list of Wi-Fi stations
+  memset(&adapter_sta_list, 0, sizeof(adapter_sta_list)); // Clearing the list of TCP/IP stations
+
+  esp_wifi_ap_get_sta_list(&wifi_sta_list); // Getting the list of Wi-Fi stations
+  tcpip_adapter_get_sta_list(&wifi_sta_list, &adapter_sta_list);
+
+  for (int i = 0; i < adapter_sta_list.num; i++)
+  {
+
+    tcpip_adapter_sta_info_t station = adapter_sta_list.sta[i]; // Get the station information
+
+    Serial.print("station nr ");
+    Serial.println(i); // Print the station number
+
+    Serial.print("MAC: ");
+
+    for (int i = 0; i < 6; i++) 
+    {
+
+      Serial.printf("%02X", station.mac[i]); // Print the MAC address of the station
+      if (i < 5)
+        Serial.print(":");
+    }
+
+    Serial.print("\nIP: "); 
+    Serial.println(station.ip.addr); // Print the IP address of the station
+  }
+
+  Serial.println("-----------");
+  delay(5000);
+}
+
 void setup() {
   Serial.begin(115200);
   SPI.begin();
-  //------------------------//
+  WiFi.softAP("MyESP32AP"); // Sets ESP32 to be a soft Wi-Fi Acess Point with the name "MyESP32AP"
+  bluetoothSetup();
+
   leitor = new LeitorRFID(&rfidBase);
-  //------------------------//  
+
 }
+
+
 void loop() {
-  Serial.println("Lendo Cartao:");
-  leitor->leCartao();
-  if(leitor->cartaoFoiLido()){
-    Serial.println(leitor->tipoCartao());
-    Serial.println(leitor->cartaoLido());
-    leitor->resetarLeitura();
-    delay(1000);
+  bluetoothSearch();
+  delay(1000);
+  wifiSearch();
+  for(int i = 0; i<100; i++) {
+    Serial.println("Lendo Cartao:");
+    leitor->leCartao();
+    if(leitor->cartaoFoiLido()){
+      Serial.println(leitor->tipoCartao());
+      Serial.println(leitor->cartaoLido());
+      leitor->resetarLeitura();
+      delay(1000);
+    }
   }
-  // Serial.println(SS);
-  // Serial.println(MOSI);
-  // Serial.println(MISO);
-  // Serial.println(SCK);
-  // delay(10000000000000);
- 
 }
