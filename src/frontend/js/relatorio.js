53784d50152f0735f11190a0e7b112ec4449c6de
manumorais <emanuele.martins@sou.inteli.edu.br>
2022-11-17 13:56:21 -0300

    edição pag relatorio

--- src/frontend/js/relatorio.js
+++ src/frontend/js/relatorio.js
@@ -36,8 +36,11 @@ const renderAdmins = (list) => {
           <td class="fs-6">${tipo}</td>
           <td class="fs-6">${patrimonio}</td>
           <td class="fs-6 d-none d-md-table-cell">${sala}</td>
+          <td class="fs-6"><button class="buttonLog" id="${id}">Clique</button></td>
           </tr>`;
         })
       : (table.innerHTML = ``);
   };
+  
+
   
