35d41e5aa2b1536946c9cf360c405acc8e965acd
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-11-23 10:53:53 -0300

    Update logs.js

--- src/backend/src/Controller/logs.js
+++ src/backend/src/Controller/logs.js
@@ -1,40 +1,31 @@
 import { openDb } from "../logs.js";
 
-
-export async function updateDeviceFromJson(json) {
-    const obj = JSON.parse(json);
-    console.log(obj[0]);
-    for (let i = 0; i <= obj.length; i++) {
-        updateStatusfromAddr(obj[i]);
-    }
-}
-
 //create table
 export async function createTable() {
     openDb().then(db => {
-        db.exec('CREATE TABLE IF NOT EXISTS Devices ( id INTEGER PRIMARY KEY, tipo TEXT, patrimonio INTEGER, localizacao INTEGER, endereco TEXT, status TEXT ) ');
+        db.exec('CREATE TABLE IF NOT EXISTS LOGS ( id INTEGER PRIMARY KEY, time INTEGER, date TEXT, localizacao INTEGER, patrimonio INTEGER, endereco TEXT, status TEXT, responsavel TEXT ) ');
     })
 }
 
 //get all devices
-export async function selectDevices(req, res) {
+export async function selectLogs(req, res) {
     return openDb().then(async (db) => {
-        const res = await db.all('SELECT * FROM Devices')
+        const res = await db.all('SELECT * FROM LOGS')
         return res;
     });
 }
 
 //get device by id
-export async function selectDevicefromAddr(addr) {
+export async function selectLogsfromAddr(addr) {
     return openDb().then(async (db) => {
-        const res = await db.get('SELECT * FROM Devices WHERE endereco=?', [addr]);
+        const res = await db.get('SELECT * FROM LOGS WHERE endereco=?', [addr]);
         return res;
     });
 }
 
 //insert device
-export async function insertDevice(device) {
+export async function insertLog(log) {
     openDb().then(db => {
-        db.run('INSERT INTO Devices (tipo, patrimonio, localizacao, endereco, status) VALUES (?,?,?,?,?)', [device.tipo, device.patrimonio, device.localizacao, device.endereco, device.status]);
+        db.run('INSERT INTO LOGS (time, date, localizacao, patrimonio, endereco, status, responsavel) VALUES (?,?,?,?,?,?,?)', [log.time, log.date, log.localizacao, log.patrimonio, log.endereco, log.status, log.responsavel]);
     })
 } 
