81ed0e418d9704d0a9371989af5e52061ed34b36
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-11-23 11:02:45 -0300

    update logs in app.js

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,12 +1,14 @@
 import { openDb } from './configDB.js';
-import { createTable, insertDevice, updateDevice, selectDevices, deleteDevice, updateDeviceFromJson, updateStatusfromAddr, selectDevicefromAddr  } from './Controller/device.js';
+import { openLogsDb } from ".configDB.js";
+import { createTable, insertDevice, updateDevice, selectDevices, deleteDevice, updateDeviceFromJson, updateStatusfromAddr, selectDevicefromAddr } from './Controller/device.js';
+import { createLogTable, insertLog, selectLogs, selectLogsfromAddr } from './Controller/logs.js';
 import fetch from "node-fetch";
 
 import express from 'express';
 const app = express();
 app.use(express.json());
 
-import {router} from './routes.js';
+import { router } from './routes.js';
 app.use(router);
 
 //LOADS CSS AND JS FILES (DON'T FORGET)
@@ -16,42 +18,42 @@ createTable();
 
 const PORT = 3000;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('Hello World');
 })
 
-app.get('/test', function(req, res){
+app.get('/test', function (req, res) {
     res.json({
         "statusCode": 200,
         "mensagem": "Teste de conexão com o servidor"
     });
 })
 
-app.get('/devices', async function(req, res){
+app.get('/devices', async function (req, res) {
     let devices = await selectDevices();
     res.json(devices);
 })
 
-app.get('/device/:id', async function(req, res){
+app.get('/device/:id', async function (req, res) {
     let id = req.params.id;
     let device = await selectDevices(id);
     res.json(device);
 })
 
-app.post('/device', function(req, res){
+app.post('/device', function (req, res) {
     insertDevice(req.body);
     res.json({
         "statusCode": 200
     });
 });
 
-app.put('/device/:id', async function(req, res){
-    if(req.body && !req.body.id){
+app.put('/device/:id', async function (req, res) {
+    if (req.body && !req.body.id) {
         res.json({
-            "statusCode" : "400",
-            "msg":"Você precisa informar um id"
+            "statusCode": "400",
+            "msg": "Você precisa informar um id"
         })
-    }else{
+    } else {
         await updateDevice(req.body)
         res.json({
             "statusCode": 200
@@ -60,7 +62,7 @@ app.put('/device/:id', async function(req, res){
 })
 
 
-app.delete('/device/:id', async (req, res)=>{
+app.delete('/device/:id', async (req, res) => {
     try {
         await deleteDevice(req.body.id);
         res.json({
@@ -85,7 +87,7 @@ app.get('/test_device', async (req, res) => {
     /*Tratar os dados aqui*/
     //res.json(devices);
     const obj = JSON.parse(devices);
-    for(let i = 0; i <= obj.length; i++){
+    for (let i = 0; i <= obj.length; i++) {
         updateStatusfromAddr(obj[i]);
     }
     res.json({
@@ -94,13 +96,13 @@ app.get('/test_device', async (req, res) => {
 });
 
 
-app.post('/test_device', function(req, res){
+app.post('/test_device', function (req, res) {
     console.log(req.body);
     res.send('ok');
 });
 
 //---------------------------------------------------------------------------------------
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('http://localhost:' + PORT);
 });
