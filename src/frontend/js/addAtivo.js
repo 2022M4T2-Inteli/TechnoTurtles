1c0160b4d94d22efacdcb918c73edbaa0acc9721
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-11-23 11:39:38 -0300

    add device

--- src/frontend/js/addAtivo.js
+++ src/frontend/js/addAtivo.js
@@ -2,3 +2,29 @@ const PORT = 3000;
 const url = `http://localhost:${PORT}`;
 
 //add new device
+let patrimonio = document.getElementById("patrimonio");
+let tipo = document.getElementById("tipo");
+let endereco = document.getElementById("endereco");
+let status = document.getElementById("status");
+
+
+const insertDevice = () => {
+    let device = {
+        patrimonio: patrimonio.value,
+        tipo: tipo.value,
+        endereco: endereco.value,
+        status: status.value
+    }
+    try{
+        axios
+            .post(`${url}/device`, device)
+            .then((res) => {
+                alert("Dispositivo cadastrado com sucesso");
+            })
+            .catch((err) => console.error(err));
+    }catch(err){
+        console.error(err);
+        alert("Preencha todos os campos");
+
+    }
+}
