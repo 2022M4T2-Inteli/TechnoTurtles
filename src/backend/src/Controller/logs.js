5321ceb6d746fcdffa1e57326127b1830b164b43
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2022-11-23 10:59:16 -0300

    Update logs.js

--- src/backend/src/Controller/logs.js
+++ src/backend/src/Controller/logs.js
@@ -2,14 +2,14 @@ import { openLogsDb } from "../configDB.js";
 
 //create table
 export async function createTable() {
-    openDb().then(db => {
+    openLogsDb().then(db => {
         db.exec('CREATE TABLE IF NOT EXISTS LOGS ( id INTEGER PRIMARY KEY, time INTEGER, date TEXT, localizacao INTEGER, patrimonio INTEGER, endereco TEXT, status TEXT, responsavel TEXT ) ');
     })
 }
 
 //get all devices
 export async function selectLogs(req, res) {
-    return openDb().then(async (db) => {
+    return openLogsDb().then(async (db) => {
         const res = await db.all('SELECT * FROM LOGS')
         return res;
     });
@@ -17,7 +17,7 @@ export async function selectLogs(req, res) {
 
 //get device by id
 export async function selectLogsfromAddr(addr) {
-    return openDb().then(async (db) => {
+    return openLogsDb().then(async (db) => {
         const res = await db.get('SELECT * FROM LOGS WHERE endereco=?', [addr]);
         return res;
     });
@@ -25,7 +25,7 @@ export async function selectLogsfromAddr(addr) {
 
 //insert device
 export async function insertLog(log) {
-    openDb().then(db => {
+    openLogsDb().then(db => {
         db.run('INSERT INTO LOGS (time, date, localizacao, patrimonio, endereco, status, responsavel) VALUES (?,?,?,?,?,?,?)', [log.time, log.date, log.localizacao, log.patrimonio, log.endereco, log.status, log.responsavel]);
     })
 } 
